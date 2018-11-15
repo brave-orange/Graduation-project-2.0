1eab042f2169b2a2f69d823cb107d93fe3d2b76b
yongcheng <951254229@qq.com>
2018-11-15 15:05:44 +0800

    wtf???

--- Application/Runtime/Cache/Admin/71433f1290cc164b549c31c545d4fdbb.php
+++ Application/Runtime/Cache/Admin/71433f1290cc164b549c31c545d4fdbb.php
@@ -4,9 +4,9 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Table</title>
-		<link rel="stylesheet" href="/test/Public/plugins/layui/css/layui.css" media="all" />
-		<link rel="stylesheet" href="/test/Public/css/global.css" media="all">
-		<link rel="stylesheet" href="/test/Public/plugins/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="/Public/plugins/layui/css/layui.css" media="all" />
+		<link rel="stylesheet" href="/Public/css/global.css" media="all">
+		<link rel="stylesheet" href="/Public/plugins/font-awesome/css/font-awesome.min.css">
 	</head>
 
 	<body>
@@ -41,7 +41,7 @@
 				</div>
 			</fieldset>
 		</div>
-		<script type="text/javascript" src="/test/Public/plugins/layui/layui.js"></script>
+		<script type="text/javascript" src="/Public/plugins/layui/layui.js"></script>
 		<script>
 			layui.use(['layer','form'], function() {
 				var form = layui.form(),
